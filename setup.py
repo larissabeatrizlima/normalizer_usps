fcfc483ef83ba2d9298c47822a02f1fd049982a9
Larissa Lima <71884306+larissabeatrizlima@users.noreply.github.com>
2024-10-10 13:37:52 -0300

    v1

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='normalizer_usps',  # Unique package name
+    version='0.1.0',         # Update version number appropriately
+    description='A library for normalizing US addresses.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # This ensures that PyPI renders your README correctly
+    url='https://github.com/yourusername/normalizer_usps',  # Replace with your repository URL
+    author='Larissa Lima',
+    author_email='larissabeatrizlima@outlook    .com',
+    license='MIT',  # Choose an appropriate license
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'normalizer_usps': ['data/*.json']},
+    install_requires=[
+        'pandas',
+        'importlib_resources; python_version < "3.9"',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        # Add other classifiers from https://pypi.org/classifiers/
+    ],
+    python_requires='>=3.6',
+)
